60999831f2d26dca83638e41204f8ee69542adc3
Pham Nguyen Tan Kiet <ptankiet1712@gmail.com>
2024-09-15 08:10:54 +0700

    checkWinner with Data2

--- Part2_/part2_Coding_Challenge#1.js
+++ Part2_/part2_Coding_Challenge#1.js
@@ -13,12 +13,21 @@ const calcAverage = (score1, score2, score3) => (score1 + score2 + score3) / 3;
 const avgDol = calcAverage(44, 23, 71); 
 const avgKoa = calcAverage(65, 54, 49);   
 
+const avgDol1 = calcAverage(85, 54, 41); 
+const avgKoa1 = calcAverage(23, 34, 27);  
+
 console.log(avgDol);
 console.log(avgKoa);
+console.log(avgDol1);
+console.log(avgKoa1);
 
 
 
 // 3. Create a function 'checkWinner' that takes the average score of each team
+// as parameters ('avgDolhins' and 'avgKoalas'), and then logs the winner
+// to the console, together with the victory points, according to the rule above.
+// Example: "Koalas win (30 vs. 13)"
+
 const checkWinner = (avgDol, avgKoa) => {
     if (avgDol >= 2 * avgKoa) {
       console.log(`Dolphins win (${avgDol} vs. ${avgKoa})`);  
@@ -29,9 +38,16 @@ const checkWinner = (avgDol, avgKoa) => {
     }
   };
   checkWinner(avgDol, avgKoa);
-// as parameters ('avgDolhins' and 'avgKoalas'), and then logs the winner
-// to the console, together with the victory points, according to the rule above.
-// Example: "Koalas win (30 vs. 13)"
 // 4. Use the 'checkWinner' function to determine the winner for both Data 1 and
 // Data 2
+const checkWinner1 = (avgDol1, avgKoa1) => {
+    if (avgDol >= 2 * avgKoa) {
+      console.log(`Dolphins win (${avgDol1} vs. ${avgKoa1})`);  
+    } else if (avgKoa1 >= 2 * avgDol) {
+      console.log(`Koalas win (${avgKoa1} vs. ${avgDol1})`);
+    } else {
+      console.log("No team wins!");
+    }
+  };
+  checkWinner(avgDol1, avgKoa1);
 // 5. Ignore draws this time
