3464aaa2f7973cf1c567cfe8ecae5c10ee44fc55
Pham Nguyen Tan Kiet <ptankiet1712@gmail.com>
2024-09-15 08:27:30 +0700

    Chỉnh sửa lại hàm calcTip

--- Part2_/part2_Coding_Challenge#2.js
+++ Part2_/part2_Coding_Challenge#2.js
@@ -8,10 +8,10 @@
 // the code from first tip calculator challenge if you need to). Use the function
 // type you like the most. Test the function using a bill value of 100
 
-const bill = 100;
-const calcTip = (bill >= 50 && bill <= 300) ? bill * 0.15 : bill * 0.20;// điều kiện nếu bill trong khoảng 50-300 thì tip = 15%., không thì 20%
+    
+const calcTip = bill => bill* (bill >= 50 && bill <= 300) ? bill * 0.15 : bill * 0.20;// điều kiện nếu bill trong khoảng 50-300 thì tip = 15%., không thì 20%
 
-console.log(calcTip);
+// console.log(calcTip);
 
 
 // 2. And now let's use arrays! So create an array 'bills' containing the test data
@@ -20,5 +20,8 @@ const bills = [125, 555, 44];
 
 // 3. Create an array 'tips' containing the tip value for each bill, calculated from
 // the function you created before
+const tips = [calcTip(bills[0]), calcTip(bills[1]), calcTip(bills[2])];
+console.log(tips); 
+
 // 4. Bonus: Create an array 'total' containing the total values, so the bill + tip
 // Test data: 125, 555 and 44
