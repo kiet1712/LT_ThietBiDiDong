cfdc97cfa6680cc4383c49723e64d08af93f2200
Pham Nguyen Tan Kiet <ptankiet1712@gmail.com>
2024-09-15 08:29:46 +0700

    Create an array 'total'

--- Part2_/part2_Coding_Challenge#2.js
+++ Part2_/part2_Coding_Challenge#2.js
@@ -25,3 +25,5 @@ console.log(tips);
 
 // 4. Bonus: Create an array 'total' containing the total values, so the bill + tip
 // Test data: 125, 555 and 44
+const total = [bills[0] + tips[0], bills[1] + tips[1], bills[2] + tips[2]];
+console.log(total);
